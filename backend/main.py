b66aab778d7a867352d3ec58d0d8c41642d48d77
Mohana Prabha <mohanaprabhakmp@gmail.com>
2026-04-20 21:35:56 +0530

    final project

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend.database import Base, engine
 from backend.routes import auth_routes, task_routes
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_routes.router)
 app.include_router(task_routes.router)
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
